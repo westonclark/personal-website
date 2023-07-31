4d13e439da5bb8d9b661983faaf407ed2220a259
Weston Clark <westoclark@gmail.com>
2023-07-31 16:23:31 -0700

    att about header

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -74,11 +74,11 @@ export default function Home() {
           </ul>
         </header>
 
-        <main className="pt-16 lg:w-1/2 lg:py-28">
+        <main className="pt-12 lg:w-1/2 lg:py-28">
           <section className="mb-16 scroll-mt-16 md:mb-24 lg:mb-36 lg:scroll-mt-24">
-            {/* <div className="sticky top-0 z-20 -mx-6 mb-4 w-screen bg-black/90 px-6 py-5 md:-mx-12 md:px-12 lg:sr-only lg:relative lg:top-auto lg:mx-auto lg:w-full lg:px-0 lg:py-0 lg:opacity-0">
+            <div className="sticky top-0 z-20 -mx-6 mb-4 w-screen bg-black/90 px-6 py-5 md:-mx-12 md:px-12 lg:sr-only lg:relative lg:top-auto lg:mx-auto lg:w-full lg:px-0 lg:py-0 lg:opacity-0">
               <h2 className="text-sm font-bold uppercase tracking-widest text-slate-200 lg:sr-only">About</h2>
-            </div> */}
+            </div>
             <p className="mb-4">
               I am a full stack Software Engineer and Audio Engineer based in Portland Oregon. I specialize in building custom tools that break down technical barriers and increase the capacity for
               developers to think creatively.
