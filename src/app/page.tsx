4645295d64bce9015536e179cb82c9e43ac99229
Weston Clark <westoclark@gmail.com>
2023-08-01 15:49:01 -0700

    update resume icon link

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -63,7 +63,7 @@ export default function Home() {
             </li>
 
             <li className="mr-5 text-xs">
-              <Link href="https://www.linkedin.com/in/westoclark/">
+              <Link href="Resume.pdf">
                 <span className="sr-only">LinkedIn</span>
                 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentcolor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
                   <path d="M14 2H6a2 2 0 0 0-2 2v16c0 1.1.9 2 2 2h12a2 2 0 0 0 2-2V8l-6-6z" />
