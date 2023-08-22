912e6e5c630e7dd9098b351d4398de7bda7af577
Weston Clark <westoclark@gmail.com>
2023-08-22 11:14:24 -0700

    update resume link

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -64,7 +64,7 @@ export default function Home() {
 
             <li className="mr-5 text-xs">
               <Link href="Resume2.pdf">
-                <span className="sr-only">LinkedIn</span>
+                <span className="sr-only">Resume</span>
                 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentcolor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
                   <path d="M14 2H6a2 2 0 0 0-2 2v16c0 1.1.9 2 2 2h12a2 2 0 0 0 2-2V8l-6-6z" />
                   <path d="M14 3v5h5M16 13H8M16 17H8M10 9H8" />
