804974aa3e9d7b8bd47748d8994ab1c6dc700410
Weston Clark <westoclark@gmail.com>
2024-01-15 14:35:39 -0800

    remove theme provider import

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,5 @@
 import './globals.css';
 import { Inter } from 'next/font/google';
-import { ThemeProvider } from '../components/ui/theme-provider';
 
 const inter = Inter({ subsets: ['latin'] });
 
