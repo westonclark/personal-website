ad74040bd0fc880398db9ea1b41492f3706ca19c
Weston Clark <westoclark@gmail.com>
2024-01-16 09:37:53 -0800

    update text format add highlihg colors

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -78,11 +78,13 @@ export default function Home() {
           {/* ABOUT ME */}
           <section className="mb-16 scroll-mt-16 md:mb-24 lg:mb-36 lg:scroll-mt-24">
             <div className="sticky top-0 z-20 -mx-6 mb-4 w-screen bg-slate-900/75 px-6 py-5 backdrop-blur md:-mx-12 md:px-12 lg:sr-only lg:relative lg:top-auto lg:mx-auto lg:w-full lg:px-0 lg:py-0 lg:opacity-0">
-              <h2 className="text-sm font-bold uppercase tracking-widest lg:sr-only">About</h2>
+              <h2 className="text-sm font-bold uppercase tracking-widest text-slate-200 lg:sr-only">About</h2>
             </div>
             <div>
               <p className="mb-4">
-                I&apos;m a Full Stack Software Engineer based in Portland Oregon. My specialty is building web applications and mobile apps using TypeScript , React.js, Node.js, React Native and Expo.
+                I&apos;m a Full Stack Software Engineer based in Portland Oregon. My specialty is building web applications and mobile apps using{' '}
+                <span className="font-medium text-slate-200 hover:text-teal-300 focus-visible:text-teal-300">TypeScript, React.js, Node.js, React Native</span> and{' '}
+                <span className="font-medium text-slate-200 hover:text-teal-300 focus-visible:text-teal-300">Expo</span>.
               </p>
               <p>When I&apos;m not coding, you can find me recording and producing music or deep in the latest fantasy novel.</p>
             </div>
@@ -91,7 +93,7 @@ export default function Home() {
           {/* EXPERIENCE */}
           <section className="mb-16 scroll-mt-16 md:mb-24 lg:mb-36 lg:scroll-mt-24">
             <div className="sticky top-0 z-20 -mx-6 mb-4 w-screen bg-inherit px-6 py-5 backdrop-blur md:-mx-12 md:px-12 lg:sr-only lg:relative lg:top-auto lg:mx-auto lg:w-full lg:px-0 lg:py-0 lg:opacity-0">
-              <h2 className="text-sm font-bold uppercase tracking-widest lg:sr-only">Experience</h2>
+              <h2 className="text-sm font-bold uppercase tracking-widest text-slate-200 lg:sr-only">Experience</h2>
             </div>
             <div>
               <ol className="group/list">
