da0d31a7bfa6a46d6a1eaf4a984f3f5d827588ad
Weston Clark <westoclark@gmail.com>
2024-01-22 09:06:31 -0800

    make resume link open in new page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -63,7 +63,7 @@ export default function Home() {
             </li>
 
             <li className="mr-5 text-xs">
-              <Link href="resume.pdf">
+              <Link href="resume.pdf" target="_blank" rel="noreferrer noopener">
                 <span className="sr-only">Resume</span>
                 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentcolor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
                   <path d="M14 2H6a2 2 0 0 0-2 2v16c0 1.1.9 2 2 2h12a2 2 0 0 0 2-2V8l-6-6z" />
